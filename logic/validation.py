3e44c5968b4431fee834f1ebb873b43374e09e22
gregormelching-lang <gregor.melching@gmail.com>
2026-08-10 15:15:17 +0200

    feat: add outlier detection to data validation

--- logic/validation.py
+++ logic/validation.py
@@ -7,10 +7,16 @@ def validate_values(values: dict) -> dict:
     for year in values:
         for metric_name in values[year]:
             if "Value" not in values[year][metric_name].keys(): flags[year][metric_name] = "missing"
-            elif metric_name not in exceptions and values[year][metric_name]["Value"] < 0: flags[year][metric_name] = "negative"
-            else: flags[year][metric_name] = "valid"
+            else:
+                if metric_name not in exceptions and values[year][metric_name]["Value"] < 0: flags[year][metric_name] = "negative"
+                else: flags[year][metric_name] = "valid"
+                if year-1 in values.keys() and "Value" in values[year-1][metric_name].keys():
+                    if not values[year-1][metric_name]["Value"] == 0:
+                        if abs((values[year][metric_name]["Value"] - values[year-1][metric_name]["Value"])/abs(values[year-1][metric_name]["Value"])) > 0.5: flags[year][metric_name] = "outlier"
+                    elif abs(values[year][metric_name]["Value"] - values[year-1][metric_name]["Value"]) > 0: flags[year][metric_name] = "outlier"
+
     return flags
 
 if __name__ == "__main__":
-    values = clean_values(get_values(1, "jpmorgan", metrics))
+    values = clean_values(get_values(4, "apple", metrics))
     print(validate_values(values))
